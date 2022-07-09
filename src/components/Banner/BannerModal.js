0694d6692effc5d9147ab2b04e356ecfee05cfd1
Deeraj <deerajrajesh22@gmail.com>
2022-07-09 22:51:07 +0530

    completed modal in banner

--- src/components/Banner/BannerModal.js
+++ src/components/Banner/BannerModal.js
@@ -23,16 +23,18 @@ export default function BannerModal({movie}) {
         <div className='view'
             style={{
                     backgroundSize: "cover",
-                    backgroundImage: `linear-gradient(rgba(0,0,0,0.9),transparent), url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+                    backgroundImage: `linear-gradient( rgba(0,0,0,12),transparent), url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
                     backgroundPosition: "center center",
                 }}
         >   
             <img className='view-poster' src={`${baseURL}${ movie.poster_path}`} alt={movie?.title} />
             <h2 className='view-title'>{movie?.title == null ? movie?.name : movie?.title}</h2>
             <div className='view-info'>
-                <p>⭐<span>{movie?.vote_average.toFixed(1)}</span></p>
+                <p>⭐<span>{movie?.vote_average.toFixed(1)}/10</span></p>
+                <p className='view-date'>{movie?.release_date == null ? movie?.first_air_date : movie?.release_date}</p>
             </div>
             <h1 className='view-desc'>{movie?.overview}</h1>
+        
         </div>
       </Modal>
     </div>
